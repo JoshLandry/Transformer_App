8fc955a2538da9490d3a91e3d88aab383ee84705
Josh <bluemazaro@yahoo.com>
2015-05-05 03:38:48 -0700

    added browserify, still having problems with fs

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,11 +3,14 @@
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-simple-mocha');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-browserify');
 
   grunt.initConfig({
     jshint: {
       dev: {
-        options:{
+        options: {
           node: true,
           globals: {
             describe: true,
@@ -16,19 +19,46 @@ module.exports = function(grunt) {
             after: true
           }
         },
-      src: ['Gruntfile.js',
-            'lib/**/*.js',
-            'test/**/*.js',
-            'index.js']
+        src: ['Gruntfile.js', 'lib/**/*.js', 'test/**/*/js']
       }
     },
+
     simplemocha: {
       all: {
         src: ['test/**/*.js']
       }
-    }
+    },
+
+    clean: {
+      build: {
+        src: ['build/']
+      }
+    },
+
+   copy: {
+     build: {
+       expand: true,
+       cwd: 'app/',
+       src: ['**/*.html','**/*.css', '**/*.jpg', '**/*.png' ],
+       dest: 'build/',
+       flatten: false,
+       filter: 'isFile'
+     }
+   },
+
+   browserify: {
+     dev: {
+       src: ['app/**/*.js', 'app/*.js'],
+       dest: 'build/bundle.js'
+     },
+
+     options: {
+       transform: ['debowerify']
+     }
+   }
   });
 
-  grunt.registerTask('test', ['jshint:dev', 'simplemocha:all']);
+  grunt.registerTask('test', ['jshint', 'simplemocha']);
+  grunt.registerTask('build', ['clean', 'browserify', 'copy']);
   grunt.registerTask('default', ['test']);
 };
